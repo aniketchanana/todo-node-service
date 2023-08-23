3e7f1c5820fd684e0c0919955bcbdc1501310546
Aniket <aniketchanana@gmail.com>
2023-08-24 01:00:32 +0530

    removed dist

--- dist/src/constants/whiteListedClientDomains.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.domains = void 0;
-exports.domains = ["http://localhost:3000"];
-//# sourceMappingURL=whiteListedClientDomains.js.map
--- dist/src/data/source/init.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./postgres/init");
-require("./postgres/models/initTable");
-//# sourceMappingURL=init.js.map
